a81f4d4349785767490395a4108cdb5c53de8605
Ronnie Cheung <ronniefcheung@gmail.com>
2015-07-10 09:20:03 -0400

    Included helpers.js.

--- build/js/app.js
+++ build/js/app.js
@@ -5,3 +5,7 @@ angular.module('controllers', []);
 angular.module('directives', []);
 
 var app = angular.module('portfolio', ['services', 'classes', 'filters', 'directives', 'controllers']);
+
+app.constant('config', {
+	'application_name': 'portfolio'
+});
---
+++ build/js/services/helpers.js
@@ -0,0 +1,17 @@
+angular.module('services').service("HelperService", function() {
+	this.loadArrayWithType = function(array, type) {		
+		var type_specific = [];
+
+		if(array != null && array.length > 0) {
+			for(var i = 0; i < array.length; i++) {
+				if(array[i] instanceof type)
+					type_specific.push(array[i]);
+			}
+		}
+
+		if(array instanceof type)
+			type_specific = [ array ];
+
+		return type_specific;
+	}
+});
